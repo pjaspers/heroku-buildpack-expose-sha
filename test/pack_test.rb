eeef01c97050e91b49e11a60bda6e6c48d0d2ee5
pjaspers <piet@10to1.be>
2016-05-02 22:49:04 +0200

    Test if we get a sha out of it.

--- test/pack_test.rb
+++ test/pack_test.rb
@@ -2,11 +2,23 @@ ENV["RACK_ENV"] = "test"
 require "minitest/autorun"
 require "minitest/pride"
 require "hatchet"
+require "pry"
+
+def git_sha_of_shacksample
+  git_dir = File.expand_path("../../repos/rack/shacksample/.git", __FILE__)
+  `git --git-dir #{git_dir} log --pretty="%H"`.strip
+end
 
 class PackTest < Minitest::Test
+  def test_it_can_find_the_sha_of_our_sample
+    assert git_sha_of_shacksample.length > 0
+  end
+
   def test_deploy
     Hatchet::Runner.new("shacksample").deploy do |app|
-      puts app.output
+      app.run("bash") do |bash|
+        bash.run("echo $SHA") { |result| assert_match git_sha_of_shacksample, result }
+      end
     end
   end
 end
